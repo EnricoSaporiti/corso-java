818ccb9469a60da8893486753ee61158b1caf70e
enric <enric@DESKTOP-1A0O46S.mshome.net>
2022-04-19 22:34:46 +0200

    concurrent

--- CorsoJava/src/it/corso/java/thread/prove/Main2.java
+++ CorsoJava/src/it/corso/java/thread/prove/Main2.java
@@ -1,9 +1,41 @@
 package it.corso.java.thread.prove;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main2 {
 
 	public static void main(String[] args) {
+		
+		Main2 m = new Main2();
+		m.concorrenza();
 
+}
+
+	private void concorrenza() {
+		
+		int coreCount = Runtime.getRuntime().availableProcessors();
+		ExecutorService service = Executors.newFixedThreadPool(coreCount);
+		
+		for( int i =0 ; i<100; i++) {
+			service.execute(new Task());
+		}
+
+		System.out.println( " Processori " + coreCount );
+		System.out.println( " Thread in esecuzione " + Thread.currentThread().getName());
+		System.out.println( " Thread in esecuzione " + Thread.activeCount() );
+
+	}
+	
+	private class Task implements Runnable {
+
+		@Override
+		public void run() {
+
+			System.out.println("In esecuzione il Task " + Thread.currentThread().getName() + " in stato " +Thread.currentThread().getState() );
+			
+		}
+		
 	}
 
 }
